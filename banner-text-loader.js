32f84a6458c0c8ff751f9b899f3384efe93ccd15
Peter Bellaby <bellaby1993@hotmail.com>
2022-02-12 18:30:39 +0900

    index page updates using fetch

---
+++ banner-text-loader.js
@@ -0,0 +1,13 @@
+//a demonstration of async await functionality
+//the text for the marquee banner, which needs to be repeated to prevent
+//odd behaviour on wide screens, is pulled and filled into the three p elements automatically
+async function getText() {
+    let text = await((await fetch("assets/banner-text.txt")).text());
+    let marqueePs = document.getElementsByClassName("banner-text-p");
+
+    for(p of marqueePs) {
+        p.innerHTML = text;
+    }
+}
+
+getText();
