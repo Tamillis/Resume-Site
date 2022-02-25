c13ffe5abbe817312a74f207566d2a2e19336783
Peter Bellaby <bellaby1993@hotmail.com>
2022-02-25 22:02:46 +0900

    Started flirting with vue

---
+++ dataViewer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "templates/meta-data.html" ?>
+    <title>Data Viewer</title>
+    <script src="https://unpkg.com/vue@3"></script>
+    <script src="src/vueApp.js" defer></script>
+</head>
+
+<body>
+
+<section class="section">
+    <h1 class="main-title">Survey Statistics</h1>
+    <p>This page is built using Vue!</p>
+    <div id="app">{{ message }}</div>
+</section>
+
+</body>
+
+</html>
